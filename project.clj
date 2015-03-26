7d62c09e05d7920388a7c0c124bddbdd1b640076
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-03-26 10:07:02 +0100

    adapt namespace to new name

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "Apache Mesos type conversions to and from Clojure"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
-  :main mesomatic.types
   :dependencies [[org.clojure/clojure    "1.6.0"]
                  [org.apache.mesos/mesos "0.21.1"]])
