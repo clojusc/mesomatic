c38df54e586027583606194e52aefb81119f5b13
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-03-26 14:31:28 +0100

    async-executor: use convenience macro

--- async/src/mesomatic/async/executor.clj
+++ async/src/mesomatic/async/executor.clj
@@ -5,29 +5,35 @@
 (defn executor
   ([ch close?]
    (e/executor
-    (reify
-      e/Executor
-      (disconnected [this driver]
-        (put! ch {:type :disconnected :driver driver}))
-      (error [this driver message]
-        (put! ch {:type :error :driver driver :message message}))
-      (framework-message [this driver data]
-        (put! ch {:type :framework-message :driver driver :data data}))
-      (kill-task [this driver task-id]
-        (put! ch {:type :kill-task :driver driver :task-id task-id}))
-      (launch-task [this driver task]
-        (put! ch {:type :launch-task :driver driver :task task}))
-      (registered [this driver executor-info framework-info slave-info]
-        (put! ch {:type           :registered
-                  :driver         driver
-                  :executor-info  executor-info
-                  :framework-info framework-info
-                  :slave-info     slave-info}))
-      (reregistered [this driver slave-info]
-        (put! ch {:type :reregistered :slave-info slave-info}))
-      (shutdown [this driver]
-        (put! ch {:type :shutdown :driver driver})
-        (when close? (close! ch))))))
+    (disconnected
+     [this driver]
+     (put! ch {:type :disconnected :driver driver}))
+    (error
+     [this driver message]
+     (put! ch {:type :error :driver driver :message message}))
+    (framework-message
+     [this driver data]
+     (put! ch {:type :framework-message :driver driver :data data}))
+    (kill-task
+     [this driver task-id]
+     (put! ch {:type :kill-task :driver driver :task-id task-id}))
+    (launch-task
+     [this driver task]
+     (put! ch {:type :launch-task :driver driver :task task}))
+    (registered
+     [this driver executor-info framework-info slave-info]
+     (put! ch {:type           :registered
+               :driver         driver
+               :executor-info  executor-info
+               :framework-info framework-info
+               :slave-info     slave-info}))
+    (reregistered
+     [this driver slave-info]
+     (put! ch {:type :reregistered :slave-info slave-info}))
+    (shutdown
+     [this driver]
+     (put! ch {:type :shutdown :driver driver})
+     (when close? (close! ch)))))
   ([ch]
    (executor ch true))
   ([]
