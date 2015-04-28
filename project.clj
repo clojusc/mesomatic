71692cdc6a419e55995fd909a697af204ac7f42f
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-04-28 13:15:55 +0200

    use latest clojure.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Apache Mesos type conversions to and from Clojure"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure    "1.7.0-beta1"]
+  :dependencies [[org.clojure/clojure    "1.7.0-beta2"]
                  [org.apache.mesos/mesos "0.22.0"]])
