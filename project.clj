d80974806788ed3727f860a4aed810a0c29275f0
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-05-26 09:51:11 +0200

    project: update libs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.spootnik/mesomatic "0.22.0-r2"
+(defproject org.spootnik/mesomatic "0.22.1-r1"
   :description "Apache Mesos type conversions to and from Clojure"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure    "1.7.0-beta2"]
-                 [org.apache.mesos/mesos "0.22.0"]])
+  :dependencies [[org.clojure/clojure    "1.7.0-RC1"]
+                 [org.apache.mesos/mesos "0.22.1"]])
