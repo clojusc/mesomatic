8245414f76399aefe7870eafc889683c66712102
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-07-30 09:33:56 +0200

    async: fix dependencies

--- async/project.clj
+++ async/project.clj
@@ -4,4 +4,4 @@
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure    "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.spootnik/mesomatic "0.23.0-r0"]])
+                 [spootnik/mesomatic     "0.23.0-r0"]])
