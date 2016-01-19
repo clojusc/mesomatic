11877e97f0698e5bd957173330f2eea1dc410310
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-01-19 14:46:30 +0100

    get ready for 0.26.0

--- async/project.clj
+++ async/project.clj
@@ -1,7 +1,7 @@
-(defproject spootnik/mesomatic-async "0.23.0-r0"
+(defproject spootnik/mesomatic-async "0.26.0-r0"
   :description "asynchronous facade for mesomatic"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure    "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [spootnik/mesomatic     "0.23.0-r0"]])
+                 [org.clojure/core.async "0.2.374"]
+                 [spootnik/mesomatic     "0.26.0-r0"]])
