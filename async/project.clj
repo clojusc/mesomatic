7a17a72e89953c03fde9d960f216354379717b43
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-02-04 18:15:29 +0100

    bump dependencies

--- async/project.clj
+++ async/project.clj
@@ -1,7 +1,7 @@
-(defproject spootnik/mesomatic-async "0.26.0-r0"
+(defproject spootnik/mesomatic-async "0.26.0-r1"
   :description "asynchronous facade for mesomatic"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure    "1.7.0"]
+  :dependencies [[org.clojure/clojure    "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [spootnik/mesomatic     "0.26.0-r0"]])
+                 [spootnik/mesomatic     "0.26.0-r1"]])
