295109faf907cc172b5e7af72df2f4092d03abc2
Aaron Probus <pair+aaron@fundingcircle.com>
2016-02-20 14:24:20 -0800

    Wraps the driver for all scheduler functions
    This change will allow the scheduler to use the wrapped driver, rather
    than using the raw mesos driver.

--- src/mesomatic/scheduler.clj
+++ src/mesomatic/scheduler.clj
@@ -28,41 +28,90 @@
   (start!                  [this])
   (stop!                   [this] [this failover?]))
 
+(defn wrap-driver [d]
+  (reify SchedulerDriver
+    (abort! [this]
+      (pb->data (.abort d)))
+    (decline-offer [this offer-id]
+      (pb->data (.declineOffer d (->pb :OfferID offer-id))))
+    (decline-offer [this offer-id filters]
+      (pb->data (.declineOffer d
+                               (->pb :OfferID offer-id)
+                               (->pb :Filters filters))))
+    (join! [this]
+      (pb->data (.join d)))
+    (kill-task! [this task-id]
+      (pb->data (.killTask d (->pb :TaskID task-id))))
+    (launch-tasks! [this offer-id tasks]
+      (pb->data (.launchTasks d
+                              (if (sequential? offer-id)
+                                (mapv (partial ->pb :OfferID) offer-id)
+                                (vector (->pb :OfferID offer-id)))
+                              (mapv (partial ->pb :TaskInfo) tasks))))
+    (launch-tasks! [this offer-id tasks filters]
+      (pb->data (.launchTasks d
+                              (if (sequential? offer-id)
+                                (mapv (partial ->pb :OfferID) offer-id)
+                                (vector (->pb :OfferID offer-id)))
+                              (mapv (partial ->pb :TaskInfo) tasks)
+                              (->pb :Filters filters))))
+    (reconcile-tasks [this statuses]
+      (pb->data
+        (.reconcileTasks d (mapv (partial ->pb :TaskStatus) statuses))))
+    (request-resources [this requests]
+      (pb->data
+        (.requestResources d (mapv (partial ->pb :Request) requests))))
+    (revive-offers [this]
+      (pb->data (.reviveOffers d)))
+    (run-driver! [this]
+      (pb->data (.run d)))
+    (send-framework-message! [this executor-id slave-id data]
+      (pb->data (.sendFrameworkMessage d
+                                       (->pb :ExecutorID executor-id)
+                                       (->pb :SlaveID slave-id)
+                                       data)))
+    (start! [this]
+      (pb->data (.start d)))
+    (stop! [this]
+      (pb->data (.stop d)))
+    (stop! [this failover?]
+      (pb->data (.stop this (boolean failover?))))))
+
 (defn wrap-scheduler
   [implementation]
   (reify
     org.apache.mesos.Scheduler
     (registered [this driver framework-id master-info]
       (registered implementation
-                  driver
+                  (wrap-driver driver)
                   (pb->data framework-id)
                   (pb->data master-info)))
     (reregistered [this driver master-info]
-      (reregistered implementation driver (pb->data master-info)))
+      (reregistered implementation (wrap-driver driver) (pb->data master-info)))
     (disconnected [this driver]
-      (disconnected implementation driver))
+      (disconnected implementation (wrap-driver driver)))
     (resourceOffers [this driver offers]
-      (resource-offers implementation driver (mapv pb->data offers)))
+      (resource-offers implementation (wrap-driver driver) (mapv pb->data offers)))
     (offerRescinded [this driver offer-id]
-      (offer-rescinded implementation driver (pb->data offer-id)))
+      (offer-rescinded implementation (wrap-driver driver) (pb->data offer-id)))
     (statusUpdate [this driver status]
-      (status-update implementation driver (pb->data status)))
+      (status-update implementation (wrap-driver driver) (pb->data status)))
     (frameworkMessage [this driver executor-id slave-id data]
       (framework-message implementation
-                         driver
+                         (wrap-driver driver)
                          (pb->data executor-id)
                          (pb->data slave-id)
                          data))
     (slaveLost [this driver slave-id]
-      (slave-lost implementation driver (pb->data slave-id)))
+      (slave-lost implementation (wrap-driver driver) (pb->data slave-id)))
     (executorLost [this driver executor-id slave-id status]
       (executor-lost implementation
-                     driver
+                     (wrap-driver driver)
                      (pb->data executor-id)
                      (pb->data slave-id)
                      (pb->data status)))
     (error [this driver message]
-      (error implementation driver message))))
+      (error implementation (wrap-driver driver) message))))
 
 (defmacro scheduler
   [& body]
