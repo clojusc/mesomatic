a0b61027a87928c8927a3d4616569715b1708a22
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-05-04 11:05:18 +0200

    project: bump dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject spootnik/mesomatic "0.27.1-r1"
+(defproject spootnik/mesomatic "0.28.0-r0"
   :description "Apache Mesos type conversions to and from Clojure"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure    "1.8.0"]
-                 [org.apache.mesos/mesos "0.27.1"]])
+                 [org.apache.mesos/mesos "0.28.0"]])
