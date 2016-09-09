8452a7427508d286d6ec6d532388a7b91a1fa601
Duncan McGreggor <oubiwann@gmail.com>
2016-09-09 15:30:23 -0500

    Fix executor registered (#27)
    * Mirror the behaviour of the Java library.
    
    This may be a risky change, as other code/projects could theoretically depend
    upon the old behaviour (I'm pretty sure it was broken, regardless).
    
    This addresses issue #8, but may need more work (depending upon impact).
    
    * Updated method names to match current Proto.java.
    
    * Fixed typo (:driver -> driver) in reregistered.
    
    * Fixed the direction of conversion.
    
    Also updated the method name for getting object attributes.
    
    * Status updates take (->pb :TaskStatus ...), so no need for conversion.
    
    There may be a better way to do that, though ...

--- src/mesomatic/types.clj
+++ src/mesomatic/types.clj
@@ -547,7 +547,7 @@
    (.getHostname info)
    (.getPort info)
    (mapv pb->data (.getResourcesList info))
-   (mapv pb->data (.getAttributes info))
+   (mapv pb->data (.getAttributesList info))
    (when-let [id (.getId info)] (pb->data id))
    (.getCheckpoint info)))
 
