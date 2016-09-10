44439bb9e214ee59b8134768e8405ac5df7a5d93
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-09-10 08:48:35 +0200

    async: bump to 1.0.1-r0

--- async/project.clj
+++ async/project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure    "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
-                 [spootnik/mesomatic     "0.28.0-r0"]])
+                 [org.clojure/core.async "0.2.391"]
+                 [spootnik/mesomatic     "1.0.1-r0"]])
