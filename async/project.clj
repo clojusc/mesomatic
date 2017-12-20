a7a9d95b6ee7762abd14c23a5edb6ef15a833524
Duncan McGreggor <oubiwann@gmail.com>
2017-12-19 21:39:07 -0600

    Bumped version for latest changes/fixes.

--- async/project.clj
+++ async/project.clj
@@ -1,7 +1,7 @@
-(defproject clojusc/mesomatic-async "1.0.1-r0"
+(defproject clojusc/mesomatic-async "1.0.1-r1"
   :description "asynchronous facade for mesomatic"
   :url "https://github.com/clojusc/mesomatic"
   :license {:name "MIT License"}
-  :dependencies [[clojusc/mesomatic "1.0.1-r0"]
+  :dependencies [[clojusc/mesomatic "1.0.1-r1"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/clojure "1.8.0"]])
