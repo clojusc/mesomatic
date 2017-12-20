c8fa8aedae74f2ca86da8f2f7d628a423948de16
Duncan McGreggor <oubiwann@gmail.com>
2017-12-19 21:24:09 -0600

    Updated artifact for new project home.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/mesomatic "1.0.1-r0"
+(defproject clojusc/mesomatic "1.0.1-r0"
   :description "Apache Mesos type conversions to and from Clojure"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
