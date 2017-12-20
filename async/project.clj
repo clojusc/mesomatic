3b26d53051bf7c7d0e363080079e474b232bc82a
Duncan McGreggor <oubiwann@gmail.com>
2017-12-19 20:46:02 -0600

    Updated artifact for new project home.

--- async/project.clj
+++ async/project.clj
@@ -1,7 +1,7 @@
-(defproject spootnik/mesomatic-async "0.27.1-r0"
+(defproject clojusc/mesomatic-async "0.27.1-r1"
   :description "asynchronous facade for mesomatic"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure    "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
-                 [spootnik/mesomatic     "0.27.1-r0"]])
+  :dependencies [[clojusc/mesomatic "0.27.1-r1"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]])
