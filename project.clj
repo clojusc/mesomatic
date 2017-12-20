bbec0fbf5402d6443a97bcc77c793fcdac36998b
Duncan McGreggor <oubiwann@gmail.com>
2017-12-19 21:39:53 -0600

    Updated project URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojusc/mesomatic "1.0.1-r1"
   :description "Apache Mesos type conversions to and from Clojure"
-  :url "https://github.com/pyr/mesomatic"
+  :url "https://github.com/clojusc/mesomatic"
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure    "1.9.0"]
                  [org.apache.mesos/mesos "1.0.1"]])
