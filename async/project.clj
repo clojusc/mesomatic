153a20fb4af237c4eba56b9541c791f9c6da7b2e
Duncan McGreggor <oubiwann@gmail.com>
2017-12-19 21:43:25 -0600

    Use snapshot version now instead.

--- async/project.clj
+++ async/project.clj
@@ -1,7 +1,7 @@
-(defproject clojusc/mesomatic-async "1.0.1-r1"
+(defproject clojusc/mesomatic-async "1.0.1-r1-SNAPSHOT"
   :description "asynchronous facade for mesomatic"
   :url "https://github.com/clojusc/mesomatic"
   :license {:name "MIT License"}
-  :dependencies [[clojusc/mesomatic "1.0.1-r1"]
+  :dependencies [[clojusc/mesomatic "1.0.1-r1-SNAPSHOT"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/clojure "1.8.0"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/mesomatic "1.0.1-r1"
+(defproject clojusc/mesomatic "1.0.1-r1-SNAPSHOT"
   :description "Apache Mesos type conversions to and from Clojure"
   :url "https://github.com/clojusc/mesomatic"
   :license {:name "MIT License"}
